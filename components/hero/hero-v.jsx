3b9bc5dfa886e62e19a0d446c5fa01fe01715404
elkitoq <elkitoq@gmail.com>
2022-09-24 00:21:05 -0300

    hero section responsive

--- components/hero/hero-v.jsx
+++ components/hero/hero-v.jsx
@@ -5,10 +5,10 @@ export function HeroVertical(){
   return (
     <Box position={'relative'}>
         <Box bgImage={`url('/basketball-heading.jpg')`}
-          bgRepeat="no-repeat"
+          bgRepeat={{base:"no-repeat", md: 'round'}}
           height={'100vh'}
           width={'100%'}
-          objectFit={'contain'}
+          objectFit={'cover'}
         >
           <Stack
             width={'100%'}
@@ -22,23 +22,23 @@ export function HeroVertical(){
             <Text
               as={'h1'}
               color={'white'}
-              fontSize={'72px'}
+              fontSize={{base:'32px',md: '50px',xl: '72px', '2xl':'72px'}}
               fontWeight={'600'}
-              w={'40%'}
+              w={{base: '80%', md: '80%', xl:'55%', '2xl':'40%'}}
               textAlign={'center'}
             >
               Creando tu puerta a la profesionalidad
             </Text>
             <Button
               as={'a'}
-              _hover={{bgColor: '#303030',color: 'white',outline: '2px solid #e7de26'}}
+              _hover={{bgColor: '#303030',color: 'white',outline: {base: '2px solid #e7de26', '2xl': '3px solid #e7de26'}}}
               _focus={{bgColor: '#303030'}}
-              w={'10%'}
+              w={{base: '50%',md:'40%', xl:'20%', '2xl':'12%'}}
               bgColor={'#e7de26'}
               color={'#000'}
-              fontSize={'22px'}
+              fontSize={{base:'22px', '2xl': '28px'}}
               marginTop={'10px'}
-              p={6}
+              p={{base:6, '2xl': 8}}
               href={'/#contact'}
             >
               Contactanos
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,22 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  
+   
+  require('dotenv').config()
+
   let nodemailer = require('nodemailer')
 
   const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-      user: 'elkitoq@gmail.com',
+      user: process.env.user,
       pass: process.env.password,
     },
     secure: true,
   })
 
   const mailData = {
-    from: '"Edits Wild" <elkitoq@gmail.com>',
-    to: 'elkitoq@gmail.com',
+    from: `"Edits Wild" <${process.env.user}>`,
+    to: process.env.user,
     subject: `Mensaje de ${req.body.nombre} desde Edits Wild`,
     text: req.body.mensaje + " | Sent from: " + req.body.email,
     html: `<div>${req.body.mensaje}</div><p>Sent from:
